e1545a8a4d95e9265d5b4675ab9143177fed4f88
Sreenivasulu <novemberautomationbatch@gmail.com>
2023-12-16 22:17:25 +0530

    First commit

--- main2.py
+++ main2.py
@@ -2,5 +2,5 @@ import subprocess
 
 #.run("python Test_Validation/file1.py", shell=True)
 #subprocess.run("python Test_Validation/file1.py", shell=True)
-subprocess.run("python Contact_info_Raw2Bronze_validation.py", shell=True)
+subprocess.run("python Test_Validation/Contact_info_Raw2Bronze_validation.py", shell=True)
 
