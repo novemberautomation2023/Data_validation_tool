d3d8e573270b25f0f393031a1eca3c6cfe471e87
Sreenivasulu <novemberautomationbatch@gmail.com>
2024-01-14 13:07:04 +0530

    First commit

--- conftest.py
+++ conftest.py
@@ -13,7 +13,7 @@ with open(project_path+'/Config/config.json','r') as f:
 Out = {"TC_ID":[], "test_Case_Name":[], "Number_of_source_Records":[], "Number_of_target_Records":[], "Number_of_failed_Records":[],"Status":[]}
 schema= ["TC_ID", "test_Case_Name", "Number_of_source_Records", "Number_of_target_Records", "Number_of_failed_Records","Status"]
 
-
+#
 spark = SparkSession.builder \
     .master("local") \
     .config("spark.jars", '/Users/harish/Downloads/spark-3.4.1-bin-hadoop3/jars/hadoop-azure-3.3.6.jar') \
