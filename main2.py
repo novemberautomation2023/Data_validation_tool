f02cafcec2d5ce07879837c82a6216e22318114a
Sreenivasulu <novemberautomationbatch@gmail.com>
2024-01-17 19:48:05 +0530

    First commit

--- main2.py
+++ main2.py
@@ -1,4 +1,9 @@
 import subprocess
+import os
+import sys
+set_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print("set path", set_path)
+sys.path.append(set_path)
 subprocess.run("python Test_Validation/Contact_info_file2raw_validation.py", shell=True)
 #subprocess.run("python Test_Validation/Contact_info_Raw2Bronze_validation.py", shell=True)
 #subprocess.run("python Test_Validation/Contact_info_Bronze2Silver_validation.py", shell=True)
