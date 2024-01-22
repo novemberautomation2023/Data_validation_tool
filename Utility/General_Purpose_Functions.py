32d36a65884fab5d6b00f2916041fc59cb135b06
Sreenivasulu <novemberautomationbatch@gmail.com>
2024-01-22 19:23:09 +0530

    First commit

--- Utility/General_Purpose_Functions.py
+++ Utility/General_Purpose_Functions.py
@@ -266,7 +266,36 @@ def write_output(TC_ID,Test_Case_Name,Number_of_source_Records,Number_of_target_
     Out["Number_of_failed_Records"].append(Number_of_failed_Records)
 
 
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
 
+
+def flatten(df):
+    # compute Complex Fields (Lists and Structs) in Schema
+    complex_fields = dict([(field.name, field.dataType)
+                           for field in df.schema.fields
+                           if type(field.dataType) == ArrayType or type(field.dataType) == StructType])
+    while len(complex_fields) != 0:
+        col_name = list(complex_fields.keys())[0]
+        print("Processing :" + col_name + " Type : " + str(type(complex_fields[col_name])))
+
+        # if StructType then convert all sub element to columns.
+        # i.e. flatten structs
+        if (type(complex_fields[col_name]) == StructType):
+            expanded = [col(col_name + '.' + k).alias(col_name + '_' + k) for k in
+                        [n.name for n in complex_fields[col_name]]]
+            df = df.select("*", *expanded).drop(col_name)
+
+        # if ArrayType then add the Array Elements as Rows using the explode function
+        # i.e. explode Arrays
+        elif (type(complex_fields[col_name]) == ArrayType):
+            df = df.withColumn(col_name, explode_outer(col_name))
+
+        # recompute remaining Complex Fields in Schema
+        complex_fields = dict([(field.name, field.dataType)
+                               for field in df.schema.fields
+                               if type(field.dataType) == ArrayType or type(field.dataType) == StructType])
+    return df
 
 
 
